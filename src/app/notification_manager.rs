0903d1e1b18168121dee20579a11efa045ec0f96
Joseph Dalbano <dalbanojoe@gmail.com>
2021-10-14 01:13:08 -0400

    refactored notifications

--- src/app/notification_manager.rs
+++ src/app/notification_manager.rs
@@ -6,6 +6,8 @@ use winapi;
 
 type NOTIFYICONDATAW = winapi::um::shellapi::NOTIFYICONDATAW;
 
+const NOTIFICATION_ID: u32 = 3434773434;
+
 pub unsafe fn add_notification() {
     let mut nid = get_notification_icon();
     winapi::um::shellapi::Shell_NotifyIconW(winapi::um::shellapi::NIM_ADD, &mut nid); 
@@ -23,21 +25,19 @@ unsafe fn get_notification_icon() -> NOTIFYICONDATAW {
 
     let wm_mymessage = winapi::um::winuser::WM_APP + 100;
 
-    let notification_tooltip_str: &str = "Snapt";
-    let tooltip_os = OsStr::new(notification_tooltip_str); 
+    let tooltip_os = OsStr::new("Snapt"); 
     let tooltip_utf16 = tooltip_os.encode_wide().collect::<Vec<u16>>();
 
     let mut tooltip_sz: [u16; 128] = [0; 128];
     tooltip_sz[..tooltip_utf16.len()].copy_from_slice(&tooltip_utf16); 
     
-    const IMAGE_RESOURCE: &str = "main_icon";
-    let ico_resource: Vec<u16> = OsStr::new(IMAGE_RESOURCE).encode_wide(). chain(Some(0).into_iter()).collect();
+    let ico_resource: Vec<u16> = OsStr::new("main_icon").encode_wide(). chain(Some(0).into_iter()).collect();
     let module_handle = winapi::um::libloaderapi::GetModuleHandleA(0 as *const i8);
 
     NOTIFYICONDATAW {
         cbSize: size_of::<winapi::um::shellapi::NOTIFYICONDATAW>() as u32,
         hWnd: hwnd,
-        uID: 1001,
+        uID: NOTIFICATION_ID,
         uCallbackMessage: wm_mymessage,
         hIcon: winapi::um::winuser::LoadIconW(module_handle, ico_resource.as_ptr()),
         szTip: tooltip_sz,
